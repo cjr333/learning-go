49de038de00ba7a5c528230a1f68f4ae61f9bba0
jay.333 <jay.333@kakaocorp.com>
2024-03-27 09:39:21 +0900

    ch4 shadowing variable

--- ch3/slice.go
+++ ch3/slice.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var x []int
-	fmt.Println(len(x))
-	y := append(x, 1, 2, 3)
+	x := append([]int{}, 1, 2, 3)
 	fmt.Println(x, len(x), cap(x))
+
+	y := append(x, 4)
 	fmt.Println(y, len(y), cap(y))
-	//y = append(y, 4)
-	//fmt.Println(y, len(y), cap(y))
-	y = append(y, 4, 5, 7, 8)
-	fmt.Println(y, len(y), cap(y))
+
+	z := append(x, 4, 5, 7, 8)
+	fmt.Println(z, len(z), cap(z))
+
+	i := append(y, 4, 5, 7, 8)
+	fmt.Println(i, len(i), cap(i))
 }
